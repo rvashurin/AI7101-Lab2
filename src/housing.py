670eb8aa752da5b3cb20e17c986f09cfbc3a826e
MHRasmy <55324562+MHRasmy@users.noreply.github.com>
2025-08-29 20:28:42 +0000

    Merge main into add_third_poly and resolve conflicts in src/housing.py

--- src/housing.py
+++ src/housing.py
@@ -25,14 +25,10 @@ LINEAR_GRID = {
 MODELS = {
     "simple_elastic": {
         "pipeline": Pipeline(BASE_PIPELINE + [("model", ElasticNet(max_iter=1000))]),
-<<<<<<< HEAD
-        "param_grid": LINEAR_GRID,
-=======
         "param_grid": {
             "model__alpha": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
             "model__l1_ratio": [0.0, 0.5, 1.0],
         },
->>>>>>> main
     },
     "poly_elastic_3": {
         "pipeline": Pipeline(
@@ -52,14 +48,10 @@ MODELS = {
                 ("model", ElasticNet(max_iter=1000)),
             ]
         ),
-<<<<<<< HEAD
-        "param_grid": LINEAR_GRID,
-=======
         "param_grid": {
             "model__alpha": [0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1.0, 10.0],
             "model__l1_ratio": [0.0, 0.5, 1.0],
         },
->>>>>>> main
     },
     "knn": {
         "pipeline": Pipeline(BASE_PIPELINE + [("model", KNeighborsRegressor())]),
